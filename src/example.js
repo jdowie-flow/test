4d786ac9381060aaf395f864ba3f62f43adde97c
Joshua Dowie <jdowie@Joshuas-Laptop.local>
2024-05-01 14:12:57 +0100

    Publishing test file

---
+++ src/example.js
@@ -0,0 +1,6 @@
+const random_number = Math.floor(Math.random() * 10);
+if (random_number % 2 === 0) {
+  console.log(`${random_number} is even`);
+} else {
+  console.log(`${random_number} is odd`);
+}
